46f4d5bc49be3301123967b0ba16f74d7f18b7f5
Dwight Nodolf <dnodolf@gmail.com>
2020-05-22 18:19:33 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,14 @@
 require 'pry'
 
+<<<<<<< HEAD
 require_relative '../lib/concerns/findable.rb'
 require_relative '../lib/concerns/memorable.rb'
 require_relative '../lib/concerns/paramble.rb'
+=======
+#require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/memorable'
+#require_relative '../lib/concerns/paramable'
+
+>>>>>>> a149ca2c63aac3889e99bd69171978e2cef18cec
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
